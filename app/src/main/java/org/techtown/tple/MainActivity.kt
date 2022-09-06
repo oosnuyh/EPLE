7204b2dcb4d1ac7e46eefe030c941fa0272028a8
oosnuyh <dkdlelanjgkfrk@naver.com>
2022-09-06 18:36:36 +0900

    Initial commit

---
+++ app/src/main/java/org/techtown/tple/MainActivity.kt
@@ -0,0 +1,56 @@
+package org.techtown.tple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private val fl: ConstraintLayout by lazy{
+        findViewById(R.id.main_layout)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.b_b)
+
+        supportFragmentManager.beginTransaction().add(fl.id, HomeFragment()).commit()
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            changeFragment(
+                when (item.itemId) {
+                    R.id.menu_home -> {
+                        bottomNavigationView.itemIconTintList = ContextCompat.getColorStateList(this, R.color.color_home)
+                        bottomNavigationView.itemTextColor = ContextCompat.getColorStateList(this, R.color.color_home)
+                        HomeFragment()
+                    }
+                    R.id.menu_search -> {
+                        bottomNavigationView.itemIconTintList = ContextCompat.getColorStateList(this, R.color.color_search)
+                        bottomNavigationView.itemTextColor = ContextCompat.getColorStateList(this, R.color.color_search)
+                        SearchFragment()
+                    }
+
+                    else -> {
+                        bottomNavigationView.itemIconTintList = ContextCompat.getColorStateList(this, R.color.color_profile)
+                        bottomNavigationView.itemTextColor = ContextCompat.getColorStateList(this, R.color.color_profile)
+                        ProfileFragment()
+                    }
+                }
+            )
+            true
+        }
+        bottomNavigationView.selectedItemId = R.id.menu_home
+    }
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.main_layout, fragment)
+            .commit()
+    }
+}
